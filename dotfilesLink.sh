e184a97272070359b0cd587ef48a9dff5fff0983
tarky <taisuke.araki@gmail.com>
2015-03-04 18:27:01 +0900

    add .gemrc in order not to download gem document

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.gemrc ~/.gemrc
