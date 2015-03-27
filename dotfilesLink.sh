8dfe20dfcd12eba1d8195b7896427e91aad7dc2c
tarky <taisuke.araki@gmail.com>
2015-03-27 22:05:56 +0900

    add tmux.conf

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.gemrc ~/.gemrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
